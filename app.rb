322533adada1a1b54c49c3ff27e49d0b9f9eebc1
javonesm89 <javosmith12@gmail.com>
2020-10-11 13:30:01 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,18 +1,12 @@
-require 'sinatra/base'
+require_relative './config/environment'
 
 class App < Sinatra::Base
-    ​
-        get "/newteam" do 
-            erb :newteam
-        end 
-    ​
-        post "/newteam" do 
-            erb :newteam
-        end 
-    ​
-        post "/team" do 
-            @team = [@name = params[:name], @coach = params[:coach], @pg = params[:pg], @sg = params[:sg], @sf = params[:sf], @pf = params[:pf], @c = params[:c]]
-            erb :team 
-        end 
-    ​
+    get '/newteam' do
+        erb :newteam
     end
+
+    post '/team' do
+        @params
+        erb :team
+    end
+end
